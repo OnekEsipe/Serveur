8004194dd5fb7432a9057f48a37784ba0ede9989
Rodolphe <Rodolphe@Asus>
2018-02-06 21:17:59 +0100

    ajout d'annotation dans les classes

--- src/main/java/com/onek/service/EvenementServiceImpl.java
+++ src/main/java/com/onek/service/EvenementServiceImpl.java
@@ -3,10 +3,12 @@ package com.onek.service;
 import java.io.Serializable;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.onek.dao.EvenementDao;
 import com.onek.model.Evenement;
 
+@Service
 public class EvenementServiceImpl implements EvenementService, Serializable{
 	private static final long serialVersionUID = 1L;
 	
