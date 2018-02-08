201aaba55e6b2c8986a618f48b97edfd88e15b10
julien <julien@MSI>
2018-02-08 22:34:51 +0100

    Front de la page de modif d'un évenement + pas mal de back de cette vue

---
+++ src/main/java/com/onek/service/EventAccueilServiceImpl.java
@@ -0,0 +1,29 @@
+package com.onek.service;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.onek.dao.EventAccueilDao;
+import com.onek.model.Candidat;
+import com.onek.model.Utilisateur;
+
+@Service
+public class EventAccueilServiceImpl implements EventAccueilService, Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Autowired
+	private EventAccueilDao eventAccueilDao;
+	
+	@Override
+	public List<Candidat> listCandidatsByEvent(int idevent){
+		return eventAccueilDao.listCandidatsByEvent(idevent);
+	}
+
+	@Override
+	public List<Utilisateur> listJurysByEvent() {
+		return eventAccueilDao.listJurysByEvent();
+	}
+}
