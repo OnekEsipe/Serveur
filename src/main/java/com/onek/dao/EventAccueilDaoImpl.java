af6e260093d97139b219bde569e310e9c8962e6b
julien <julien@MSI>
2018-02-08 17:40:35 +0100

    Début du front accueil evenement + debut back

---
+++ src/main/java/com/onek/dao/EventAccueilDaoImpl.java
@@ -0,0 +1,12 @@
+package com.onek.dao;
+
+import java.io.Serializable;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class EventAccueilDaoImpl implements EventAccueilDao, Serializable {
+	private static final long serialVersionUID = 1L;
+
+
+}
