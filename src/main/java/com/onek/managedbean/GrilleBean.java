36f2cbfbead5c4f03a885b2ffdffbd3f7b8a3246
Rayco <Rayco@172.22.131.157>
2018-02-09 14:25:12 +0100

    Modification du bean suite a mise a jour BDD

--- src/main/java/com/onek/managedbean/GrilleBean.java
+++ src/main/java/com/onek/managedbean/GrilleBean.java
@@ -1,5 +1,6 @@
 package com.onek.managedbean;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,6 +26,7 @@ public class GrilleBean {
 	private List<Integer> numbers = new ArrayList<>();
 	
 	private Evenement event;
+	private final BigDecimal ref = new BigDecimal(0);
 
 	@PostConstruct
 	public void postInit() {
@@ -36,14 +38,14 @@ public class GrilleBean {
 	private int nbDescripteur;
 
 	private String nom;
-	private double coefficient;
+	private BigDecimal coefficient;
 	private String categorie;
 
-	private double poids1;
-	private double poids2;
-	private double poids3;
-	private double poids4;
-	private double poids5;
+	private BigDecimal poids1;
+	private BigDecimal poids2;
+	private BigDecimal poids3;
+	private BigDecimal poids4;
+	private BigDecimal poids5;
 	private String texte1;
 	private String texte2;
 	private String texte3;
@@ -52,65 +54,65 @@ public class GrilleBean {
 
 	public void onClicAdd() {
 		event = (Evenement) FacesContext.getCurrentInstance().getExternalContext().getRequestMap().get("event");
-//		Critere c = new Critere();
-//		c.setEvenement(event);
-//		c.setCategorie(categorie);
-//		c.setCoefficient(coefficient);
-//		c.setTexte(nom);
-//		Descripteur d;
-//		if (poids1 != 0 && texte1 != null) {
-//			d = new Descripteur();
-//			d.setNiveau('A');
-//			d.setPoids(poids1);
-//			d.setTexte(texte1);
-//			d.setCritere(c);
-//			c.addDescripteur(d);
-//		}
-//		if (poids2 != 0 && texte2 != null) {
-//			d = new Descripteur();
-//			d.setNiveau('B');
-//			d.setPoids(poids2);
-//			d.setTexte(texte2);
-//			d.setCritere(c);
-//			c.addDescripteur(d);
-//		}
-//		if (poids3 != 0 && texte3 != null) {
-//			d = new Descripteur();
-//			d.setNiveau('C');
-//			d.setPoids(poids3);
-//			d.setTexte(texte3);
-//			d.setCritere(c);
-//			c.addDescripteur(d);
-//		}
-//		if (poids4 != 0 && texte4 != null) {
-//			d = new Descripteur();
-//			d.setNiveau('D');
-//			d.setPoids(poids4);
-//			d.setTexte(texte4);
-//			d.setCritere(c);
-//			c.addDescripteur(d);
-//		}
-//		if (poids5 != 0 && texte5 != null) {
-//			d = new Descripteur();
-//			d.setNiveau('E');
-//			d.setPoids(poids5);
-//			d.setTexte(texte5);
-//			d.setCritere(c);
-//			c.addDescripteur(d);
-//		}
-//		criteres.add(c);
-//		resetValues();
+		Critere c = new Critere();
+		c.setEvenement(event);
+		c.setCategorie(categorie);
+		c.setCoefficient(coefficient);
+		c.setTexte(nom);
+		Descripteur d;
+		if (poids1.compareTo(ref) > 0 && texte1 != null) {
+			d = new Descripteur();
+			d.setNiveau("A");
+			d.setPoids(poids1);
+			d.setTexte(texte1);
+			d.setCritere(c);
+			c.addDescripteur(d);
+		}
+		if (poids2.compareTo(ref) > 0 && texte2 != null) {
+			d = new Descripteur();
+			d.setNiveau("B");
+			d.setPoids(poids2);
+			d.setTexte(texte2);
+			d.setCritere(c);
+			c.addDescripteur(d);
+		}
+		if (poids3.compareTo(ref) > 0 && texte3 != null) {
+			d = new Descripteur();
+			d.setNiveau("C");
+			d.setPoids(poids3);
+			d.setTexte(texte3);
+			d.setCritere(c);
+			c.addDescripteur(d);
+		}
+		if (poids4.compareTo(ref) > 0 && texte4 != null) {
+			d = new Descripteur();
+			d.setNiveau("D");
+			d.setPoids(poids4);
+			d.setTexte(texte4);
+			d.setCritere(c);
+			c.addDescripteur(d);
+		}
+		if (poids5.compareTo(ref) > 0 && texte5 != null) {
+			d = new Descripteur();
+			d.setNiveau("E");
+			d.setPoids(poids5);
+			d.setTexte(texte5);
+			d.setCritere(c);
+			c.addDescripteur(d);
+		}
+		criteres.add(c);
+		resetValues();
 	}
 
 	private void resetValues() {
 		categorie = "";
-		coefficient = 0;
+		coefficient = ref;
 		nom = "";
-		poids1 = 0;
-		poids2 = 0;
-		poids3 = 0;
-		poids4 = 0;
-		poids5 = 0;
+		poids1 = ref;
+		poids2 = ref;
+		poids3 = ref;
+		poids4 = ref;
+		poids5 = ref;
 		texte1 = "";
 		texte2 = "";
 		texte3 = "";
@@ -135,11 +137,11 @@ public class GrilleBean {
 		this.nom = nom;
 	}
 
-	public double getCoefficient() {
+	public BigDecimal getCoefficient() {
 		return coefficient;
 	}
 
-	public void setCoefficient(double coeffecient) {
+	public void setCoefficient(BigDecimal coeffecient) {
 		this.coefficient = coeffecient;
 	}
 
@@ -151,43 +153,43 @@ public class GrilleBean {
 		this.categorie = categorie;
 	}
 
-	public double getPoids1() {
+	public BigDecimal getPoids1() {
 		return poids1;
 	}
 
-	public void setPoids1(double poids1) {
+	public void setPoids1(BigDecimal poids1) {
 		this.poids1 = poids1;
 	}
 
-	public double getPoids2() {
+	public BigDecimal getPoids2() {
 		return poids2;
 	}
 
-	public void setPoids2(double poids2) {
+	public void setPoids2(BigDecimal poids2) {
 		this.poids2 = poids2;
 	}
 
-	public double getPoids3() {
+	public BigDecimal getPoids3() {
 		return poids3;
 	}
 
-	public void setPoids3(double poids3) {
+	public void setPoids3(BigDecimal poids3) {
 		this.poids3 = poids3;
 	}
 
-	public double getPoids4() {
+	public BigDecimal getPoids4() {
 		return poids4;
 	}
 
-	public void setPoids4(double poids4) {
+	public void setPoids4(BigDecimal poids4) {
 		this.poids4 = poids4;
 	}
 
-	public double getPoids5() {
+	public BigDecimal getPoids5() {
 		return poids5;
 	}
 
-	public void setPoids5(double poids5) {
+	public void setPoids5(BigDecimal poids5) {
 		this.poids5 = poids5;
 	}
 
