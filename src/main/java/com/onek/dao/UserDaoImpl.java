fdabfa8a2753765a0655157cae92b75667f0d8da
Rayco <Rayco@192.168.0.10>
2018-02-13 16:10:49 +0100

    Ajout utilisateur OK , supression pas OK

--- src/main/java/com/onek/dao/UserDaoImpl.java
+++ src/main/java/com/onek/dao/UserDaoImpl.java
@@ -4,8 +4,6 @@ import java.io.Serializable;
 
 import java.util.List;
 import java.util.ArrayList;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
