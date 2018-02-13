bd716feb453232b8692643c7beb156f0b5630840
julien <julien@MSI>
2018-02-13 13:45:21 +0100

    rectif pour faciliter le merge

--- src/main/java/com/onek/managedbean/EventAccueilBean.java
+++ src/main/java/com/onek/managedbean/EventAccueilBean.java
@@ -2,6 +2,7 @@ package com.onek.managedbean;
 
 import java.io.Serializable;
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -29,15 +30,15 @@ public class EventAccueilBean implements Serializable {
 
 	@Autowired
 	private EventAccueilService eventAccueilservice;
+
+	@Autowired
+	EvenementService evenement;
 	
 	@Autowired
 	private UserService userService;
 
-	@Autowired
-	EvenementService evenement;
-
 	private final Navigation navigation = new Navigation();
-
+	
 	private int idEvent;
 	private Evenement event;
 
@@ -58,105 +59,9 @@ public class EventAccueilBean implements Serializable {
 	private Utilisateur utilisateur;
 	private List<Utilisateur> filteredutilisateurs;
 	private Utilisateur selectedutilisateur;
-
+	
 	private PasswordGenerator passwordGenerator;
 
-	public void before(ComponentSystemEvent e) {
-		if (!FacesContext.getCurrentInstance().isPostback()) {
-			String idEventString = navigation.getURLParameter("id");
-			setIdEvent(Integer.parseInt(idEventString));
-			this.event = evenement.findById(idEvent);
-			candidats = eventAccueilservice.listCandidatsByEvent(idEvent);
-			utilisateurs = eventAccueilservice.listJurysByEvent(idEvent);
-			this.statut = event.getStatus();
-			this.dateStart = event.getDatestart();
-			this.dateEnd = event.getDatestop();
-		}
-	}
-
-	public void addJuryAnonymeButton() {
-		passwordGenerator = new PasswordGenerator();
-		List<Utilisateur> anonymousJurys = new ArrayList<>();
-		Utilisateur anonymousJury;
-		if (juryAnonyme > 0) {
-			for (int i = 0; i < juryAnonyme; i++) {
-				anonymousJury = new Utilisateur();
-				anonymousJury.setDroits("A");
-				anonymousJury.setIsdeleted(false);
-				if (i < 10) {
-					anonymousJury.setLogin("Jury00" + i + "_" + idEvent);
-					anonymousJury.setNom("Jury00" + i + "_" + idEvent);
-				}
-				if ((i >= 10) && (i < 100)) {
-					anonymousJury.setLogin("Jury0" + i + "_" + idEvent);
-					anonymousJury.setNom("Jury0" + i + "_" + idEvent);
-				}
-				if ((i >= 100) && (i < 1000)) {
-					anonymousJury.setLogin("Jury" + i + "_" + idEvent);
-					anonymousJury.setNom("Jury" + i + "_" + idEvent);
-				}
-				anonymousJury.setMotdepasse(passwordGenerator.generatePassword(8));
-				anonymousJury.setMail("");
-				anonymousJury.setPrenom("");
-				anonymousJurys.add(anonymousJury);
-			}	
-			userService.addJurysAnonymes(anonymousJurys, event);	
-			utilisateurs = eventAccueilservice.listJurysByEvent(idEvent);
-		}
-	}
-
-	public void eventUpdateButton() {
-		// Pour test
-		DateFormat dfDate = new SimpleDateFormat("dd/MM/yyyy");
-		String sDate = dfDate.format(dateStart);
-
-		DateFormat dfTime = new SimpleDateFormat("HH:mm");
-		String sTime = dfTime.format(timeStart);
-
-		message = "Statut: " + statut + " dateStart:" + dateStart + " dateStartFORMATTE:" + sDate + " dateEnd:"
-				+ dateEnd + " timeStart:" + timeStart + " timeStartFORMATTEE:" + sTime + " timeEnd:" + timeEnd;
-	}
-
-	public void supprimerCandidat() {
-		FacesContext fc = FacesContext.getCurrentInstance();
-		Map<String, String> params = fc.getExternalContext().getRequestParameterMap();
-		int idcandidat = Integer.valueOf(params.get("idcandidat"));
-
-		eventAccueilservice.supprimerCandidat(idcandidat);
-		candidats = eventAccueilservice.listCandidatsByEvent(1);
-
-	}
-
-	public void supprimerUtilisateur() {
-		FacesContext fc = FacesContext.getCurrentInstance();
-		Map<String, String> params = fc.getExternalContext().getRequestParameterMap();
-		int iduser = Integer.valueOf(params.get("iduser"));
-
-		eventAccueilservice.supprimerUtilisateur(iduser);
-		utilisateurs = eventAccueilservice.listJurysByEvent(1);
-	}
-
-	public void buttonGrille() {
-		navigation.redirect("grille.xhtml?id=" + idEvent);
-	}
-
-	public void buttonAttribution() {
-		navigation.redirect("attributionJuryCandidat.xhtml?id=" + idEvent);
-	}
-
-	public void buttonAddJury() {
-		navigation.redirect("addJury.xhtml?id=" + idEvent);
-	}
-
-	public void buttonAddCandidat() {
-		navigation.redirect("addCandidates.xhtml?id=" + idEvent);
-	}
-
-	public void buttonExport() {
-		// to do
-		// eventAccueilservice.listJurysByEvent().
-	}
-	
 	public List<Utilisateur> getFilteredutilisateurs() {
 		return filteredutilisateurs;
 	}
@@ -196,7 +101,27 @@ public class EventAccueilBean implements Serializable {
 	public void setJuryAnonyme(int juryAnonyme) {
 		this.juryAnonyme = juryAnonyme;
 	}
-	
+
+	public void before(ComponentSystemEvent e) throws ParseException {
+		if (!FacesContext.getCurrentInstance().isPostback()) {
+			Navigation navigation = new Navigation();
+			String idEventString = navigation.getURLParameter("id");
+			setIdEvent(Integer.parseInt(idEventString));
+			this.event = evenement.findById(idEvent);
+			candidats = eventAccueilservice.listCandidatsByEvent(idEvent);
+			utilisateurs = eventAccueilservice.listJurysByEvent(idEvent);
+			this.statut = event.getStatus();
+			this.dateStart = event.getDatestart();
+			this.dateEnd = event.getDatestop();
+
+			DateFormat dfTime = new SimpleDateFormat("HH:mm");
+			String sTimeStart = dfTime.format(event.getDatestart().getTime());
+			String sTimeEnd = dfTime.format(event.getDatestop().getTime());
+			timeStart = dfTime.parse(sTimeStart);
+			timeEnd = dfTime.parse(sTimeEnd);
+		}
+	}
+
 	public List<Utilisateur> getUtilisateurs() {
 		return utilisateurs;
 	}
@@ -283,6 +208,96 @@ public class EventAccueilBean implements Serializable {
 
 	public void setIdEvent(int idEvent) {
 		this.idEvent = idEvent;
+	}
+
+	public void addJuryAnonymeButton() {
+		passwordGenerator = new PasswordGenerator();
+		List<Utilisateur> anonymousJurys = new ArrayList<>();
+		Utilisateur anonymousJury;
+		if (juryAnonyme > 0) {
+			for (int i = 0; i < juryAnonyme; i++) {
+				anonymousJury = new Utilisateur();
+				anonymousJury.setDroits("A");
+				anonymousJury.setIsdeleted(false);
+				if (i < 10) {
+					anonymousJury.setLogin("Jury00" + i + "_" + idEvent);
+					anonymousJury.setNom("Jury00" + i + "_" + idEvent);
+				}
+				if ((i >= 10) && (i < 100)) {
+					anonymousJury.setLogin("Jury0" + i + "_" + idEvent);
+					anonymousJury.setNom("Jury0" + i + "_" + idEvent);
+				}
+				if ((i >= 100) && (i < 1000)) {
+					anonymousJury.setLogin("Jury" + i + "_" + idEvent);
+					anonymousJury.setNom("Jury" + i + "_" + idEvent);
+				}
+				anonymousJury.setMotdepasse(passwordGenerator.generatePassword(8));
+				anonymousJury.setMail("");
+				anonymousJury.setPrenom("");
+				anonymousJurys.add(anonymousJury);
+			}	
+			userService.addJurysAnonymes(anonymousJurys, event);	
+			utilisateurs = eventAccueilservice.listJurysByEvent(idEvent);
+		}
+	}
+
+	public void eventUpdateButton() {
+		event.setDatestart(new Date(dateStart.getTime() + timeStart.getTime()));
+		event.setDatestop(new Date(dateEnd.getTime() + timeEnd.getTime()));
+		event.setStatus(statut);
+		//eventAccueilservice.editEvenement(event);
+		// Pour test
+		/*
+		 * DateFormat dfDate = new SimpleDateFormat("dd/MM/yyyy"); String sDate =
+		 * dfDate.format(dateStart);
+		 * 
+		 * DateFormat dfTime = new SimpleDateFormat("HH:mm"); String sTime =
+		 * dfTime.format(timeStart);
+		 * 
+		 * message = "Statut: " + statut + " dateStart:" + dateStart +
+		 * " dateStartFORMATTE:" + sDate + " dateEnd:" + dateEnd + " timeStart:" +
+		 * timeStart + " timeStartFORMATTEE:" + sTime + " timeEnd:" + timeEnd;
+		 */
+	}
+
+	public void supprimerCandidat() {
+		FacesContext fc = FacesContext.getCurrentInstance();
+		Map<String, String> params = fc.getExternalContext().getRequestParameterMap();
+		int idcandidat = Integer.valueOf(params.get("idcandidat"));
+
+		eventAccueilservice.supprimerCandidat(idcandidat);
+		candidats = eventAccueilservice.listCandidatsByEvent(1);
+
+	}
+
+	public void supprimerUtilisateur() {
+		FacesContext fc = FacesContext.getCurrentInstance();
+		Map<String, String> params = fc.getExternalContext().getRequestParameterMap();
+		int iduser = Integer.valueOf(params.get("iduser"));
+
+		eventAccueilservice.supprimerUtilisateur(iduser);
+		utilisateurs = eventAccueilservice.listJurysByEvent(1);
+	}
+
+	public void buttonGrille() {
+		navigation.redirect("grille.xhtml?id=" + idEvent);
+	}
+
+	public void buttonAttribution() {
+		navigation.redirect("attributionJuryCandidat.xhtml?id=" + idEvent);
+	}
+
+	public void buttonAddJury() {
+		navigation.redirect("addJury.xhtml?id=" + idEvent);
+	}
+
+	public void buttonAddCandidat() {
+		navigation.redirect("addCandidates.xhtml?id=" + idEvent);
+	}
+
+	public void buttonExport() {
+		// to do
+		// eventAccueilservice.listJurysByEvent().
 	}
 
 }
