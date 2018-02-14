dc42fe9e61474f4a11c2b0f7d95561bdaf8f310d
julien <julien@MSI>
2018-02-14 21:52:45 +0100

    correction erreur bizarre suite rebase

---
+++ src/main/java/com/onek/service/EvaluationService.java
@@ -0,0 +1,11 @@
+package com.onek.service;
+
+import java.util.List;
+import java.util.Map;
+
+import com.onek.model.Candidat;
+import com.onek.model.Jury;
+
+public interface EvaluationService {
+	
+}
---
+++ src/main/java/com/onek/service/EvaluationServiceImpl.java
@@ -0,0 +1,17 @@
+package com.onek.service;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.onek.dao.EvaluationDao;
+import com.onek.dao.JuryDao;
+import com.onek.model.Candidat;
+import com.onek.model.Jury;
+
+public class EvaluationServiceImpl implements EvaluationService, Serializable {
+	private static final long serialVersionUID = 1L;
+	
+}
