50ae848f0a51d2af0d2652e2de2e4aafdc990937
Rayco <Rayco@192.168.0.10>
2018-02-16 10:39:22 +0100

    Export terminé mais manque quelques cas d'erreur

--- src/main/java/com/onek/managedbean/EventAccueilBean.java
+++ src/main/java/com/onek/managedbean/EventAccueilBean.java
@@ -21,7 +21,6 @@ import javax.faces.event.ComponentSystemEvent;
 
 import org.apache.poi.ss.usermodel.BorderStyle;
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.FormulaEvaluator;
 import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.util.CellAddress;
@@ -401,21 +400,37 @@ public class EventAccueilBean implements Serializable {
 	}
 
 	// PARTIE EXPORT DE L'EVENEMENT
-	Map<String, Map<String, CellAddress>> mapParam = new HashMap<>();
-	Map<Integer, String> niveaux = new HashMap<>();
-
-	private void init() {
+	private final Map<String, Map<String, CellAddress>> mapParam = new HashMap<>();
+	private final Map<Integer, String> niveaux = new HashMap<>();
+	private final Map<Candidat, Map<String, CellAddress>> resultCandidats = new HashMap<>();
+	
+	private XSSFCellStyle style;
+	private XSSFCellStyle style2;
+	
+	private void init(XSSFWorkbook workbook) {
 		niveaux.put(0, "A");
 		niveaux.put(1, "B");
 		niveaux.put(2, "C");
 		niveaux.put(3, "D");
 		niveaux.put(4, "E");
 		niveaux.put(5, "F");
+		niveaux.put(-1, "-");
+		style = workbook.createCellStyle();
+		style2 = workbook.createCellStyle();
+		style.setBorderLeft(BorderStyle.THIN);
+		style.setBorderRight(BorderStyle.THIN);
+		style.setBorderBottom(BorderStyle.THIN);
+		style.setBorderTop(BorderStyle.THIN);
+		style2.setBorderLeft(BorderStyle.THIN);
+		style2.setBorderRight(BorderStyle.THIN);
+		style2.setBorderBottom(BorderStyle.THIN);
+		style2.setBorderTop(BorderStyle.THIN);
+		style2.setAlignment(HorizontalAlignment.CENTER);
 	}
 
 	public void buildXlsx() {
-		init();
 		XSSFWorkbook workbook = new XSSFWorkbook();
+		init(workbook);
 		List<Critere> criteres = event.getCriteres();
 		List<Candidat> candidats = event.getCandidats();
 		fillParameterPage(workbook, criteres);
@@ -448,19 +463,27 @@ public class EventAccueilBean implements Serializable {
 		for (Critere critere : criteres) {
 			cell = row.createCell(colNum++);
 			cell.setCellValue(critere.getTexte());
+			cell.setCellStyle(style2);
 		}
+		cell = row.createCell(colNum++);
+		cell.setCellValue("Total");
+		cell.setCellStyle(style2);
 		for (Candidat candidat : candidats) {
 			colNum = 0;
 			row = sheet.createRow(rowNum++);
 			cell = row.createCell(colNum++);
 			cell.setCellValue(candidat.getNom() + " " + candidat.getPrenom());
+			cell.setCellStyle(style2);
 			for (Critere critere : criteres) {
 				cell = row.createCell(colNum++);
-				// cell.setCellFormula("='"+candidat.getNom()+" "+candidat.getPrenom()+"'!");
-				cell.setCellValue(""); // ICI MAP.GET(CANDIDAT).GET(TOTALCRITERE)
+				cell.setCellFormula("'"+candidat.getNom()+" "+candidat.getPrenom()+"'!"+resultCandidats.get(candidat).get(critere.getTexte()).formatAsString()); // ICI MAP.GET(CANDIDAT).GET(TOTALCRITERE)
+				cell.setCellStyle(style);
 			}
 			cell = row.createCell(colNum++);
-			cell.setCellValue(""); // ICI MAP.GET(CANDIDAT).GET(TOTAL)
+			if(resultCandidats.get(candidat).containsKey("total")) {
+				cell.setCellFormula("'"+candidat.getNom()+" "+candidat.getPrenom()+"'!"+resultCandidats.get(candidat).get("total").formatAsString());
+				cell.setCellStyle(style);
+			}
 		}
 	}
 
@@ -468,17 +491,6 @@ public class EventAccueilBean implements Serializable {
 		int colNum = 0;
 		int rowNum = 0;
 		XSSFSheet sheet = workbook.createSheet("Parametres");
-		XSSFCellStyle style = workbook.createCellStyle();
-		style.setBorderLeft(BorderStyle.THIN);
-		style.setBorderRight(BorderStyle.THIN);
-		style.setBorderBottom(BorderStyle.THIN);
-		style.setBorderTop(BorderStyle.THIN);
-		XSSFCellStyle style2 = workbook.createCellStyle();
-		style2.setBorderLeft(BorderStyle.THIN);
-		style2.setBorderRight(BorderStyle.THIN);
-		style2.setBorderBottom(BorderStyle.THIN);
-		style2.setBorderTop(BorderStyle.THIN);
-		style2.setAlignment(HorizontalAlignment.CENTER);
 		for (Critere critere : criteres) {
 			mapParam.put(critere.getTexte(), new HashMap<>());
 			Row row = sheet.createRow(rowNum);
@@ -531,9 +543,8 @@ public class EventAccueilBean implements Serializable {
 	}
 
 	private void fillCandidatesPages(XSSFWorkbook workbook, List<Critere> criteres, List<Candidat> candidats) {
-		int nbcriteres = 0;
-		int nbrows = 0;
 		for (Candidat candidat : candidats) {
+			resultCandidats.put(candidat, new HashMap<>());
 			int rowNum = 0;
 			int colNum = 0;
 			Map<String, List<String>> mapResultsByCritere = new HashMap<>();
@@ -542,23 +553,26 @@ public class EventAccueilBean implements Serializable {
 			Row row = sheet.createRow(rowNum++);
 			Cell cell = row.createCell(colNum++);
 			cell.setCellValue("Jurys");
+			cell.setCellStyle(style2);
 			for (Critere critere : criteres) {
-				nbcriteres++;
 				cell = row.createCell(colNum++);
 				cell.setCellValue("Note " + critere.getTexte());
+				cell.setCellStyle(style2);
 				cell = row.createCell(colNum++);
 				cell.setCellValue("Commentaire " + critere.getTexte());
+				cell.setCellStyle(style2);
 			}
 			cell = row.createCell(colNum++);
 			cell.setCellValue("Total");
+			cell.setCellStyle(style2);
 			for (Evaluation eval : evaluations) {
-				nbrows++;
 				row = sheet.createRow(rowNum++);
 				colNum = 0;
 				cell = row.createCell(colNum++);
 				cell.setCellValue(
 						eval.getJury().getUtilisateur().getNom() + " " + eval.getJury().getUtilisateur().getPrenom());
 				StringBuilder sb = new StringBuilder();
+				cell.setCellStyle(style2);
 				sb.append("SUM(");
 				for (Note note : eval.getNotes()) {
 					if (!mapResultsByCritere.containsKey(note.getCritere().getTexte())) {
@@ -567,11 +581,13 @@ public class EventAccueilBean implements Serializable {
 					mapResultsByCritere.get(note.getCritere().getTexte()).add(niveaux.get(note.getNiveau()));
 					cell = row.createCell(colNum++);
 					cell.setCellValue(niveaux.get(note.getNiveau()));
+					cell.setCellStyle(style2);
 					sb.append("Parametres!" + mapParam.get(note.getCritere().getTexte()).get("coef").formatAsString()
 							+ "*Parametres!" + mapParam.get(note.getCritere().getTexte())
 									.get(niveaux.get(note.getNiveau())).formatAsString());
 					cell = row.createCell(colNum++);
 					cell.setCellValue(note.getCommentaire());
+					cell.setCellStyle(style);
 					sb.append("+");
 				}
 				if (eval.getNotes().isEmpty()) {
@@ -582,12 +598,15 @@ public class EventAccueilBean implements Serializable {
 				}
 				cell = row.createCell(colNum);
 				cell.setCellFormula(sb.toString());
+				cell.setCellStyle(style);
 				colNum++;
 			}
 			colNum = 0;
 			row = sheet.createRow(rowNum);
 			cell = row.createCell(colNum++);
 			cell.setCellValue("Total");
+			cell.setCellStyle(style2);
+			List<String[]> adresses = new ArrayList<>();
 			for (Critere critere : criteres) {
 				StringBuilder sb = new StringBuilder();
 				sb.append("AVERAGE(");
@@ -600,13 +619,33 @@ public class EventAccueilBean implements Serializable {
 					sb.append(")");
 					cell = row.createCell(colNum++);
 					cell.setCellFormula(sb.toString());
+					cell.setCellStyle(style);
+					String[] values = new String[2];
+					values[0] = critere.getTexte();
+					values[1] = cell.getAddress().formatAsString();
+					adresses.add(values);
 					colNum++;
 				} else {
 					sb.setLength(0);
 					cell = row.createCell(colNum++);
 					cell.setCellValue("");
+					cell.setCellStyle(style);
 					colNum++;
 				}
+				resultCandidats.get(candidat).put(critere.getTexte(), cell.getAddress());
+			}
+			cell = row.createCell(colNum++);
+			if (!adresses.isEmpty()) {
+				StringBuilder sb = new StringBuilder();
+				sb.append("SUM(");
+				for (String[] values : adresses) {
+					sb.append(values[1]).append("*Parametres!").append(mapParam.get(values[0]).get("coef").formatAsString()).append("+");
+				}
+				sb.setLength(sb.length()-1);
+				sb.append(")");
+				cell.setCellFormula(sb.toString());
+				cell.setCellStyle(style);
+				resultCandidats.get(candidat).put("total", cell.getAddress());
 			}
 		}
 	}
