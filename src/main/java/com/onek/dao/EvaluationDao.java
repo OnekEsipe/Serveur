ef9204d11ac6eb828c6c8d35640029d9c36daa56
julien <julien@MSI>
2018-02-16 10:00:26 +0100

    Attribution jury manuel

--- src/main/java/com/onek/dao/EvaluationDao.java
+++ src/main/java/com/onek/dao/EvaluationDao.java
@@ -8,8 +8,5 @@ public interface EvaluationDao {
 	Evaluation findById(Integer id);
 	void deleteEvaluation(int idJury, int idCandidat);
 	void saveEvaluation(String nomCandidat, String prenomCandidat, int idevent, Jury jury);
-<<<<<<< HEAD
-=======
-
->>>>>>> branch 'AttribCandidatJuryBack' of https://github.com/OnekEsipe/Serveur.git
+	void update(Evaluation evaluation);
 }
