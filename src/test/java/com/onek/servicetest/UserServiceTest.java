9ca3556a49463a672cdd34a2c0ed258118d0396c
Tamela <tamelahamed@yahoo.fr>
2018-03-01 12:51:35 +0100

    correction test

--- src/test/java/com/onek/servicetest/UserServiceTest.java
+++ src/test/java/com/onek/servicetest/UserServiceTest.java
@@ -241,7 +241,7 @@ public class UserServiceTest {
 
 	@Test
 	public void testuserExistAndCorrectPasswordOK() {
-		Utilisateur user = userService.findByLogin("bb");
+		Utilisateur user = userService.findByLogin("aa");
 		assertTrue(userService.userExistAndCorrectPassword(user.getLogin(),user.getMotdepasse()));
 	}
 	
