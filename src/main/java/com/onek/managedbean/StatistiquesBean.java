e13492134f23fbea1a8b4f03105c53c8c7fbbd75
Rayco <Rayco@172.22.131.157>
2018-03-02 14:47:22 +0100

    test signature 10

--- src/main/java/com/onek/managedbean/StatistiquesBean.java
+++ src/main/java/com/onek/managedbean/StatistiquesBean.java
@@ -660,8 +660,8 @@ public class StatistiquesBean implements Serializable {
 						anchor.setRow2(rowNum-1);
 						anchor.setCol2(colNum);
 						Picture pict = drawing.createPicture(anchor, imageIDX);
-						pict.resize(3, 1);
-						sheet.setColumnWidth(colNum, 8000);
+						pict.resize(2, 1);
+						sheet.setColumnWidth(colNum, 12000);
 						row.setHeight((short) 1400);
 						colNum++;
 					}
